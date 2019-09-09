1fb70afd1ed9849bc9d3a49e5ddd1be43df587f8
Christy Burkhalter <christykburkhalter@gmail.com>
2019-09-09 00:31:27 +0000

    image blur1

--- app/models/image_blur1.rb
+++ app/models/image_blur1.rb
@@ -1,2 +1,20 @@
-class ImageBlur1 < ApplicationRecord
+class Image
+
+def initialize(array)
+  @imageblur = array
 end
+
+def output_image
+  @imageblur.each do |row|
+    puts row.join
+  end
+end
+end
+
+image = Image.new([
+  [0, 0, 0, 0],
+  [0, 1, 0, 0],
+  [0, 0, 0, 1],
+  [0, 0, 0, 0]
+  ])
+image.output_image
